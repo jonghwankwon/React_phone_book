d6bd15afc0dfda75ad53404edc8bcdc44e1fc186
Jong Hwan Kwon <47845945+jonghwankwon@users.noreply.github.com>
2020-03-11 17:06:57 +0900

    onUpdate 추가

--- src/components/PhoneInfoList.js
+++ src/components/PhoneInfoList.js
@@ -3,16 +3,20 @@ import PhoneInfo from './PhoneInfo';
 
 class PhoneInfoList extends Component {
     static defaultProps = {
-        data: []
+        data: [],
+        onRemove: () => console.warn('omRemove not defined'),
+        onUpdate: () => console.warn('onUpdate not defined'),
     }
+
     render() {
-        const { data, onRemove } = this.props;
+        const { data, onRemove, onUpdate } = this.props;
         const list = data.map(  // data 라는 배열을 가져와서 map 을 통하여 JSX 로 변환
             info => (
             <PhoneInfo 
                 key={info.id}   // key 는 리액트에서 배열을 렌더링을 할 때 꼭 필요한 값
                 info={info}
                 onRemove={onRemove}
+                onUpdate={onUpdate}
             />)
         );
         return(
